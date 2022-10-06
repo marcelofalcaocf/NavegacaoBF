4599077ce09b103fa7887a1e6213ce17b091ea47
Marcelo Falcão <marcelo_falcaoc@hotmail.com>
2022-10-06 19:46:24 -0300

    Adição das telas um e dois

--- NavegacaoBF/NavegacaoBF/ViewController.swift
+++ NavegacaoBF/NavegacaoBF/ViewController.swift
@@ -14,13 +14,5 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-    @IBAction func irTela02(_ sender: Any) {
-        performSegue(withIdentifier: "irTela02", sender: Self)
-    }
-    
-    @IBAction func irTela03(_ sender: Any) {
-        performSegue(withIdentifier: "irTela03", sender: Self)
-    }
-    
 }
 
