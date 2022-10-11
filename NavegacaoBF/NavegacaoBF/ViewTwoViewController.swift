f3277c7a9cd13b419fe326823d20e19965fc97d6
Marcelo Falcão <marcelo_falcaoc@hotmail.com>
2022-10-11 20:47:45 -0300

    criacao da life cycle na view 2

--- NavegacaoBF/NavegacaoBF/ViewTwoViewController.swift
+++ NavegacaoBF/NavegacaoBF/ViewTwoViewController.swift
@@ -11,17 +11,22 @@ class ViewTwoViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        print(#function)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewWillAppear(_ animated: Bool) {
+        print(#function)
     }
-    */
 
+    override func viewDidAppear(_ animated: Bool) {
+        print(#function)
+    }
+
+    override func viewDidDisappear(_ animated: Bool) {
+        print(#function)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        print(#function)
+    }
 }
